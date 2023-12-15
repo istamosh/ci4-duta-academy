50a3f60d01b8075a8a75142418bcd171bd97dd51
btiger-laptop <63596969+istamosh@users.noreply.github.com>
2023-12-15 20:53:52 +0700

    Added: custom routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// when first serving (using spark) you will be redirected to home::index like below value
+// when first serving (using spark) you will be redirected to Home.php => index() like below value
 $routes->get('/', 'Home::index');
+
+// custom added routes
+$routes->add('blog/data', 'Blogs::users/7');
+$routes->add('product/(:any)', 'Catalog::productLookup');
