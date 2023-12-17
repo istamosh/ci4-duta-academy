6cc562a77884ee8cd1ff9bc94516c8674af63465
istamosh <63596969+istamosh@users.noreply.github.com>
2023-12-17 10:25:54 +0700

    Added: matrix bootstrap5 template for book_list page
    Updated: include vendor/ again and removing empty inline style css (cause getting warning)

--- app/Views/book_list.php
+++ app/Views/book_list.php
@@ -1,6 +1,14 @@
-<!-- placeholder book lists -->
 <!-- redirected from app/Controller/Book.php index() -->
+
+<!-- borrowing from layouts/matrix/ -->
+<?= $this->extend('layouts/matrix') ?>
+
+<!-- define content section -->
+<?= $this->section('content') ?>
+<hr>
 <strong>Book List:</strong> <br>
 - Atomic Habits <br>
-- The Art of Not Giving A F*ck <br>
+- The Subtle Art of Not Giving A F*ck <br>
 - Meditations <br>
+<hr>
+<?= $this->endsection() ?>
